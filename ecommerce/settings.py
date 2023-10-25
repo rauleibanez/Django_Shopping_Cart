75a8b6e0d945e8eb1449bd33a1187f3ed4aaa7d5
rauleibanez <83990877+rauleibanez@users.noreply.github.com>
2023-10-24 19:05:25 -0500

    Funcionalidad de Templates, Vistas y urls

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -119,6 +119,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS= [BASE_DIR / "ecommerce/static/",]
 
+MEDIA_URL = 'images/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "ecommerce/static/images")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- store/views.py
+++ store/views.py
@@ -1,21 +1,38 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 # ---------------------------
 # Vista del Store (almacen)
 # ---------------------------
 def store(request):
-	context={}
+	products = Product.objects.all()
+	context={'products':products}
 	return render(request, 'store/store.html', context)
 # ---------------------------
 # Vista del Carrito (Cart)
 # ---------------------------
 def cart(request):
-	context={}
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		items = order.orderitem_set.all()
+	else:
+		items =[]
+		order ={'get_cart_total':0, 'get_cart_items':0}
+	context={'items':items, 'order':order}
 	return render(request, 'store/cart.html', context)
 # ---------------------------
 # Vista de la compra 
 # ---------------------------
 def checkout(request):
-	context={}
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		items = order.orderitem_set.all()
+	else:
+		# Crea un carro vacio para los usuarios no loggeados
+		items =[]
+		order ={'get_cart_total':0, 'get_cart_items':0}
+	context={'items':items, 'order':order}
 	return render(request, 'store/checkout.html', context)
